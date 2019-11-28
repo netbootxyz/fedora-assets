bc30666bfbf4e3e3c492c7565fa0f05056ea222c
thelamer <ryankuba@gmail.com>
2019-11-27 18:04:24 -0800

    adding master branch as current latest asset for workstation ISO at version 31

---
+++ settings.sh
@@ -0,0 +1,6 @@
+URL="https://download.fedoraproject.org/pub/fedora/linux/releases/31/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-31-REPLACE_VERSION.iso"
+TYPE=file
+CONTENTS="\
+isolinux/vmlinuz|vmlinuz
+isolinux/initrd.img|initrd
+LiveOS/squashfs.img|squashfs.img"
