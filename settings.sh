e35edd77529dec39230154677fff6bcfe99bd796
Antony <antony@mes.ser.li>
2020-10-27 10:50:02 -0500

    Add Fedora 33

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://download.fedoraproject.org/pub/fedora/linux/releases/32/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-32-REPLACE_VERSION.iso"
+URL="https://download.fedoraproject.org/pub/fedora/linux/releases/33/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-33-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 isolinux/vmlinuz|vmlinuz
