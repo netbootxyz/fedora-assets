16e45bab17f1537e57f4ed20cb2c911965f6acec
Antony <antony@mes.ser.li>
2020-10-27 12:11:49 -0500

    Bump to 33

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/32/Spins/x86_64/iso/Fedora-KDE-Live-x86_64-32-REPLACE_VERSION.iso"
+URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/33/Spins/x86_64/iso/Fedora-KDE-Live-x86_64-33-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 isolinux/vmlinuz|vmlinuz
