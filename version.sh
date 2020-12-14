ba287c11908d88955be8c34520f2921c1e4df597
netboot-ci <netboot-ci@netboot.xyz>
2020-12-14 05:50:51 +0000

    Updating workflow automation

---
+++ version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+VERSION=$(curl -sL https://dl.fedoraproject.org/pub/fedora/linux/releases/33/Spins/x86_64/iso/ |awk '/Fedora-LXQt-Live/' | grep -Po "(\d+\.)+\d+" | head -1)
+echo "${VERSION}"
