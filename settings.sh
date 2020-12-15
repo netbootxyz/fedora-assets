cb32f16c188ed3606b0a185df69c585f8b1d34ab
netboot-ci <netboot-ci@netboot.xyz>
2020-12-15 04:19:54 +0000

    Updating workflow automation

--- settings.sh
+++
@@ -1,6 +0,0 @@
-URL="https://download.fedoraproject.org/pub/fedora/linux/releases/31/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-31-REPLACE_VERSION.iso"
-TYPE=file
-CONTENTS="\
-isolinux/vmlinuz|vmlinuz
-isolinux/initrd.img|initrd
-LiveOS/squashfs.img|squashfs.img"
