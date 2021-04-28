51a02ef1606835af5a5c6fb5c04d90a534b5a9a0
Antony Messerli <antony@mes.ser.li>
2021-04-27 23:37:21 -0500

    Add Fedora 34-i3

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://dl.fedoraproject.org/pub/fedora/linux/releases/34/Spins/x86_64/iso/ |awk '/Fedora-Cinnamon-Live/' | grep -Po "(\d+\.)+\d+" | head -1)
+VERSION=$(curl -sL https://dl.fedoraproject.org/pub/fedora/linux/releases/34/Spins/x86_64/iso/ |awk '/Fedora-i3-Live/' | grep -Po "(\d+\.)+\d+" | head -1)
 echo "${VERSION}"
