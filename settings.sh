8ae3e69d999e90c83756034ae234cc40dbb8156f
Antony Messerli <antony@mes.ser.li>
2022-05-10 18:17:51 -0500

    Fedora 36 Workstation

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://download.fedoraproject.org/pub/fedora/linux/releases/35/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-35-REPLACE_VERSION.iso"
+URL="https://download.fedoraproject.org/pub/fedora/linux/releases/36/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-36-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 isolinux/vmlinuz|vmlinuz
