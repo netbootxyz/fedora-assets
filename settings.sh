48c13c681aca2b158bd254003f12f3c0ce0e7857
Antony Messerli <antony@mes.ser.li>
2022-05-10 19:38:13 -0500

    F36 Cinnamon

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/35/Spins/x86_64/iso/Fedora-Cinnamon-Live-x86_64-35-REPLACE_VERSION.iso"
+URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/36/Spins/x86_64/iso/Fedora-Cinnamon-Live-x86_64-35-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 isolinux/vmlinuz|vmlinuz
