bdfaccd447cfd513c46715d291c0b64f17ead749
Antony Messerli <antony@mes.ser.li>
2022-05-10 19:41:00 -0500

    F36 LXDE

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL https://dl.fedoraproject.org/pub/fedora/linux/releases/35/Spins/x86_64/iso/ |awk '/Fedora-LXDE-Live/' | grep -Po "(\d+\.)+\d+" | head -1)
+VERSION=$(curl -sL https://dl.fedoraproject.org/pub/fedora/linux/releases/36/Spins/x86_64/iso/ |awk '/Fedora-LXDE-Live/' | grep -Po "(\d+\.)+\d+" | head -1)
 echo "${VERSION}"
