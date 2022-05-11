c0f6bf579866e5cd321f8c08cb367f075613e8ad
Antony Messerli <antony@mes.ser.li>
2022-05-10 19:46:50 -0500

    F36 Xfce

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/35/Spins/x86_64/iso/Fedora-Xfce-Live-x86_64-35-REPLACE_VERSION.iso"
+URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/36/Spins/x86_64/iso/Fedora-Xfce-Live-x86_64-36-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
 isolinux/vmlinuz|vmlinuz
