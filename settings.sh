e06250082896a3dba37d11bf7188a087a8bc8f78
Antony Messerli <antony@mes.ser.li>
2022-11-16 13:34:00 -0600

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
 URL="https://download.fedoraproject.org/pub/fedora/linux/releases/37/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-37-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
-isolinux/vmlinuz|vmlinuz
-isolinux/initrd.img|initrd
+images/pxeboot/vmlinuz|vmlinuz
+images/pxeboot/initrd.img|initrd
 LiveOS/squashfs.img|squashfs.img"
