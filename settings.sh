3e7fe31ee633b732c092f4f3193b63adf1efee2c
Antony Messerli <antony@mes.ser.li>
2022-11-16 22:01:41 -0600

    Update paths

--- settings.sh
+++ settings.sh
@@ -1,6 +1,6 @@
 URL="https://dl.fedoraproject.org/pub/fedora/linux/releases/37/Spins/x86_64/iso/Fedora-Cinnamon-Live-x86_64-37-REPLACE_VERSION.iso"
 TYPE=file
 CONTENTS="\
-isolinux/vmlinuz|vmlinuz
-isolinux/initrd.img|initrd
+images/pxeboot/vmlinuz|vmlinuz
+images/pxeboot/initrd.img|initrd
 LiveOS/squashfs.img|squashfs.img"
